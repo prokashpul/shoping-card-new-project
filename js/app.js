92e4aad3e5cd8322e0fa44eb06e98b792c8105c7
prokash Pul <pprokash102@gmail.com>
2022-02-15 11:09:51 +0600

    add script file add add event plus minus button

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
-document.body.style.backgroundColor = "rgba(62, 73, 122,.90)"
+document.body.style.backgroundColor = "rgba(62, 73, 122,.90)";
+document.getElementById('main').style.backgroundColor = "rgba(62, 73, 122,.5)";
+// use function 
+function count(name, price, isTrue) {
+
+    const inputValue = document.getElementById(name + '-input');
+    if (isTrue == true) {
+        inputValue.value = parseInt(inputValue.value) + 1;
+    } else {
+        if (inputValue.value > 0) {
+            inputValue.value = parseInt(inputValue.value) - 1;
+        }
+    }
+    const newNumer = inputValue.value;
+    const singleprice = document.getElementById(name + '-price');
+    console.log(singleprice)
+    singleprice.innerText = price * newNumer;
+
+}
+//camera price 
+document.getElementById('camara-plus').addEventListener('click', function() {
+    count('camara', 1588, true)
+});
+document.getElementById('camara-minus').addEventListener('click', function() {
+    count('camara', 1588, false)
+});
+
+//lance price 
+document.getElementById('lance-plus').addEventListener('click', function() {
+    count('lance', 150, true)
+});
+document.getElementById('lance-minus').addEventListener('click', function() {
+    count('lance', 150, false)
+});
